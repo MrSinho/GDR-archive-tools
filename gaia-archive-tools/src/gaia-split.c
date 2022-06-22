944cdeea51e62ecb526439740c9a175708f552f0
Anas Boustani <sino3791@gmail.com>
2022-06-22 18:48:05 +0200

    Update gaia-split.c

--- gaia-archive-tools/src/gaia-split.c
+++ gaia-archive-tools/src/gaia-split.c
@@ -7,18 +7,9 @@ extern "C" {
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, char** argv) {//example call: ./gaia-split ../gaia-bin/  0000
+int main(int argc, char** argv) {//example call: ./gaia-split ../gaia-bin/  0
     if (argc == 3) {
         gaiaSplit(argv[1], (uint32_t)atoi(argv[2]));
-    }
-    else if (argc == 4) {
-        for (uint32_t i = (uint32_t)atoi(argv[2]); i < ((uint32_t)atoi(argv[3]) + 1); i++) {
-            gaiaConvertCSV(
-                argv[1],
-                argv[2],
-                i
-            );
-        }
     }
     else {
         printf("gaia-split error: wrong arguments\n");
